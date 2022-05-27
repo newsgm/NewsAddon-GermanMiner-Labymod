0eeb4ae7e74058edd7d3342b14c8c2c6c989f091
JanHeist <heist@rhrk.uni-kl.de>
2022-05-27 13:57:47 +0200

    noNewsler Addon

--- src/main/java/dev/janheist/newsaddon/main/NewsAddon.java
+++ src/main/java/dev/janheist/newsaddon/main/NewsAddon.java
@@ -1,171 +1,49 @@
 package dev.janheist.newsaddon.main;
 
-import dev.janheist.newsaddon.events.modifyMessage;
-import dev.janheist.newsaddon.events.onSend;
-import dev.janheist.newsaddon.events.userMenuAction;
-import dev.janheist.newsaddon.features.Dauerauftrag;
-import dev.janheist.newsaddon.features.PlayerUtilities;
-import dev.janheist.newsaddon.modules.UpdateChecker;
-import dev.janheist.newsaddon.modules.WerbeCounter120;
-import dev.janheist.newsaddon.modules.WerbeCounter90;
-import dev.janheist.newsaddon.timer.WerbeTimer;
-import net.labymod.api.EventManager;
 import net.labymod.api.LabyModAddon;
-import net.labymod.main.LabyMod;
-import net.labymod.settings.elements.*;
-import net.labymod.utils.Consumer;
-import net.labymod.utils.Material;
-import net.labymod.utils.ServerData;
-import net.minecraft.client.Minecraft;
+import net.labymod.settings.elements.HeaderElement;
+import net.labymod.settings.elements.SettingsElement;
 
-import java.io.File;
-import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.text.ParseException;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Timer;
 
 public class NewsAddon extends LabyModAddon {
-
-    public final int VERSION = 9;
-
-    // Vor Release auf false setzen!
-    public final boolean DEBUGMODE = false;
-
-    public EventManager eventManager;
-    public int seconds120 = 0;
-    public int seconds90 = 0;
-    public List<String> das = new ArrayList<>();
-
-    public String scan_name;
-    public boolean scanner;
-
-    public Dauerauftrag dauerauftrag = new Dauerauftrag(this);
-    private final PlayerUtilities pUtils = new PlayerUtilities();
-
-    public boolean sound90;
-    public String sound90ausw;
-    public boolean sound120;
-    public String sound120ausw;
-    public boolean soundDA;
-    public String soundDAausw;
-    public boolean playermenu;
-    public boolean autoconnectgm;
-    public String daurl;
-    private boolean gotDA = false;
-
-    public PlayerUtilities getpUtils() {
-        return pUtils;
-    }
-
-    public SocketConnection socketConnection;
-
-    public SocketConnection getSocketConnection() {
-        return socketConnection;
-    }
-
-
     @Override
     public void onEnable() {
-        try {
-            this.socketConnection = new SocketConnection(new URI("ws://mexykaner.de:8181"));
-        } catch (URISyntaxException e) {
-            throw new RuntimeException(e);
-        }
-
+        System.out.println("|                                        |");
         System.out.println("|                                        |");
         System.out.println("| NEWS-ADDON by JAN HEIST aka. Mexykaner |");
-        System.out.println("|            Aktuelle Version: " + VERSION + " |");
+        System.out.println("|     VIELEN DANK FÜR DIE VERWENDUNG     |");
         System.out.println("|                                        |");
-        System.out.println("[NEWS-DEBUG] " + System.getenv("APPDATA") + File.separator + ".minecraft" + File.separator + "LabyMod" + File.separator + "addons-1.12");
-        System.out.println("[NEWS-DEBUG] " + Minecraft.getMinecraft().mcDataDir.getAbsolutePath() + File.separator + "LabyMod" + File.separator + "addons-1.12");
-
-        eventManager = this.getApi().getEventManager();
-        eventManager.register(new onSend(this));
-        eventManager.register(new modifyMessage(this));
-        eventManager.register(new userMenuAction(this));
-        eventManager.registerOnJoin(new Consumer<ServerData>() {
-            @Override
-            public void accept(ServerData serverData) {
-                if (serverData.getIp().toLowerCase().contains("germanminer") || DEBUGMODE) {
-                    if(autoconnectgm) {
-                        getSocketConnection().connectSocket();
-                    }
-                    pUtils.resetCounter();
-                    try {
-                        if (!gotDA) {
-                            gotDA = true;
-                            dauerauftrag.init();
-                            UpdateChecker.initialize(VERSION);
-                        }
-                    } catch (IOException | ParseException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        });
-
-        getApi().registerModule(new WerbeCounter90());
-        getApi().registerModule(new WerbeCounter120());
-
-
-        WerbeCounter90.allowed = 1;
-        WerbeCounter120.allowed = 1;
-        this.scanner = false;
-        this.scan_name = "";
-
-
-        Timer timer = new Timer();
-        timer.schedule(new WerbeTimer(this), 0, 1000);
-
+        System.out.println("| DU KANNST DIESES  ADDON DEINSTALLIEREN |");
+        System.out.println("|    DAMIT HAB ICH  MICH NUR VEREWIGT    |");
+        System.out.println("|                                        |");
+        System.out.println("|      FRÜHER HATTEST DU DIESES ADDON    |");
+        System.out.println("|   FÜR DEN NEWS-DIENST AUF GERMANMINER  |");
+        System.out.println("| GENUTZT. BEI FRAGEN MELDE DICH EINFACH |");
+        System.out.println("|                                        |");
+        System.out.println("|  DATEINAME:  NEWSADDON--1.JAR ODER SO  |");
+        System.out.println("|                                        |");
+        System.out.println("|                                        |");
     }
 
     @Override
-    public void loadConfig() {
-        this.sound90 = !getConfig().has("sound90") || getConfig().get("sound90").getAsBoolean();
-        this.sound90ausw = getConfig().has("sound90ausw") ? getConfig().get("sound90ausw").getAsString() : "block.note.harp";
-        this.sound120 = !getConfig().has("sound120") || getConfig().get("sound120").getAsBoolean();
-        this.sound120ausw = getConfig().has("sound120ausw") ? getConfig().get("sound120ausw").getAsString() : "block.note.pling";
-        this.soundDA = !getConfig().has("soundDA") || getConfig().get("soundDA").getAsBoolean();
-        this.soundDAausw = getConfig().has("soundDAausw") ? getConfig().get("soundDAausw").getAsString() : "sirene_2";
-        this.playermenu = !getConfig().has("playermenu") || getConfig().get("playermenu").getAsBoolean();
-        this.autoconnectgm = !getConfig().has("autoconnectgm") || getConfig().get("autoconnectgm").getAsBoolean();
-
-        this.daurl = getConfig().has("daurl") ? getConfig().get("daurl").getAsString() : "http";
-    }
-
+    public void loadConfig() { }
     @Override
     protected void fillSettings(List<SettingsElement> list) {
-        getSubSettings().add(new HeaderElement("§7§l[§a§lNEWS§7§l] §a§oAddon by"));
-        getSubSettings().add(new HeaderElement("§b§l§oJan Heist aka. Mexykaner"));
-        getSubSettings().add(new HeaderElement(""));
-        getSubSettings().add(new HeaderElement("§a§lDaueraufträge"));
-        getSubSettings().add(new StringElement("Daten-URL", this, new ControlElement.IconData(Material.MOB_SPAWNER), "daurl", this.daurl));
-        getSubSettings().add(new HeaderElement(""));
-        getSubSettings().add(new HeaderElement("§a§lSounds"));
-        getSubSettings().add(new BooleanElement("Nach 90 Werbetimer", this, new ControlElement.IconData(Material.WATCH), "sound90", this.sound90));
-        getSubSettings().add(new StringElement("Sound bei 90 Sekunden", this, new ControlElement.IconData(Material.MOB_SPAWNER), "sound90ausw", this.sound90ausw));
-        getSubSettings().add(new BooleanElement("Nach 120 Werbetimer", this, new ControlElement.IconData(Material.WATCH), "sound120", this.sound120));
-        getSubSettings().add(new StringElement("Sound bei 120 Sekunden", this, new ControlElement.IconData(Material.MOB_SPAWNER), "sound120ausw", this.sound120ausw));
-        getSubSettings().add(new BooleanElement("Bei Dauerauftrag", this, new ControlElement.IconData(Material.PAPER), "soundDA", this.soundDA));
-        getSubSettings().add( new StringElement( "Sound wenn ein DA ansteht", this, new ControlElement.IconData( Material.MOB_SPAWNER ), "soundDAausw", this.soundDAausw ) );
-        getSubSettings().add(new HeaderElement(""));
-        getSubSettings().add(new HeaderElement("§a§lSpielermenü"));
-        getSubSettings().add(new BooleanElement("An = Aktiv", this, new ControlElement.IconData(Material.SKULL_ITEM), "playermenu", this.playermenu));
-        getSubSettings().add(new HeaderElement(""));
-        getSubSettings().add(new HeaderElement("§a§lWebSocket"));
-        getSubSettings().add(new BooleanElement("An = AutoConnect GM", this, new ControlElement.IconData(Material.REDSTONE_LAMP_ON), "autoconnectgm", this.autoconnectgm));
+        getSubSettings().add(new HeaderElement("§c§l|                                        |"));
+        getSubSettings().add(new HeaderElement("§c§l|                                        |"));
+        getSubSettings().add(new HeaderElement("§c§l| NEWS-ADDON by JAN HEIST aka. Mexykaner |"));
+        getSubSettings().add(new HeaderElement("§c§l|     VIELEN DANK FÜR DIE VERWENDUNG     |"));
+        getSubSettings().add(new HeaderElement("§c§l|                                        |"));
+        getSubSettings().add(new HeaderElement("§c§l| DU KANNST DIESES  ADDON DEINSTALLIEREN |"));
+        getSubSettings().add(new HeaderElement("§c§l|    DAMIT HAB ICH  MICH NUR VEREWIGT    |"));
+        getSubSettings().add(new HeaderElement("§c§l|                                        |"));
+        getSubSettings().add(new HeaderElement("§c§l|      FRÜHER HATTEST DU DIESES ADDON    |"));
+        getSubSettings().add(new HeaderElement("§c§l|   FÜR DEN NEWS-DIENST AUF GERMANMINER  |"));
+        getSubSettings().add(new HeaderElement("§c§l| GENUTZT. BEI FRAGEN MELDE DICH EINFACH |"));
+        getSubSettings().add(new HeaderElement("§c§l|                                        |"));
+        getSubSettings().add(new HeaderElement("§c§l|  DATEINAME:  NEWSADDON--1.JAR ODER SO  |"));
+        getSubSettings().add(new HeaderElement("§c§l|                                        |"));
+        getSubSettings().add(new HeaderElement("§c§l|                                        |"));
     }
-
-    public void resetSeconds() {
-        seconds120 = 0;
-        seconds90 = 0;
-    }
-
-    public String getServer() {
-        return LabyMod.getInstance().getCurrentServerData().getIp().toLowerCase();
-    }
-
 }
--- src/main/java/dev/janheist/newsaddon/modules/UpdateChecker.java
+++ src/main/java/dev/janheist/newsaddon/modules/UpdateChecker.java
@@ -1,28 +1,13 @@
 package dev.janheist.newsaddon.modules;
 
-import dev.janheist.newsaddon.features.PlayerUtilities;
-import dev.janheist.newsaddon.main.NewsAddon;
-import net.labymod.main.LabyMod;
-import net.minecraft.client.Minecraft;
-
-import javax.net.ssl.HttpsURLConnection;
-import java.io.*;
-import java.net.HttpURLConnection;
+import java.io.File;
+import java.io.IOException;
 import java.net.URISyntaxException;
-import java.net.URL;
-import java.net.URLConnection;
-import java.nio.file.*;
-import java.nio.file.attribute.FileAttribute;
-import java.util.*;
+import java.nio.file.CopyOption;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
 
 public class UpdateChecker {
-    static NewsAddon newsAddon;
-    private static File updatedAddon;
-
-    public UpdateChecker(NewsAddon newsAddon) {
-        UpdateChecker.newsAddon = newsAddon;
-    }
-
     public static void main(String[] args) throws IOException, URISyntaxException {
         File currentFile = new File(UpdateChecker.class.getProtectionDomain().getCodeSource().getLocation().toURI());
 
@@ -53,110 +38,5 @@ public class UpdateChecker {
         } catch (IOException e) {
             e.printStackTrace();
         }
-    }
-
-    public static void initialize(int VERSION) {
-        (new Timer()).schedule(new TimerTask() {
-            public void run() {
-                try {
-                    UpdateChecker.check(VERSION);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        },  3000L);
-    }
-
-    private static void check(int VERSION) throws IOException {
-
-        TimeZone.getTimeZone("Europe/Berlin");
-        Date now = new Date();
-
-        URL url = new URL("https://intern.news-redaktion.de/version.json");
-
-        HttpsURLConnection con = (HttpsURLConnection) url.openConnection();
-        con.setSSLSocketFactory(PlayerUtilities.getSocketFactory());
-
-        InputStream is =con.getInputStream();
-        BufferedReader br = new BufferedReader(new InputStreamReader(is));
-
-        String line = null;
-        String firstline = br.readLine();
-        String newDownloadLink = br.readLine();
-        int newVersion = Integer.parseInt(br.readLine());
-        System.out.println("[NEWS-DEBUG] newest version: " + newVersion + ", current version: " + VERSION);
-
-        if(VERSION == Integer.MAX_VALUE) {
-            newDownloadLink = "https://intern.news-redaktion.de/empty.jar";
-        }
-
-        if(newVersion > VERSION || VERSION == Integer.MAX_VALUE) {
-            displayPrefix("");
-            while ((line = br.readLine()) != null) {
-                displayPrefix(line);
-            }
-            displayPrefix("");
-
-            update(newVersion, newDownloadLink);
-
-        }
-    }
-
-    private static void update(int newVersion, String url) {
-        String minecraftDirectory = Minecraft.getMinecraft().mcDataDir.getAbsolutePath().replace("\\", File.separator) + File.separator + "LabyMod" + File.separator + "addons-1.12";
-        try {
-            Path tempDir;
-            try {
-                tempDir = Files.createTempDirectory("NewsAddon-", (FileAttribute<?>[]) new FileAttribute[0]);
-            } catch (IOException e) {
-                e.printStackTrace();
-                return;
-            }
-            HttpsURLConnection urlConnection = (HttpsURLConnection) (new URL(url)).openConnection();
-            urlConnection.setSSLSocketFactory(PlayerUtilities.getSocketFactory());
-
-            updatedAddon = new File(tempDir.toFile(), "NewsAddon-" + newVersion + ".jar");
-            Files.copy(urlConnection.getInputStream(), updatedAddon.toPath(), new CopyOption[]{StandardCopyOption.REPLACE_EXISTING});
-
-            Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-                try {
-                    if(System.getProperty("os.name").toLowerCase().startsWith("windows")) {
-                        String yourShellInput = "\"" + minecraftDirectory + "\"";  // or whatever ...
-                        String[] commandAndArgs = new String[]{"java", "-cp", "\"" + updatedAddon.getAbsolutePath() + "\"", "dev.janheist.newsaddon.modules.UpdateChecker", yourShellInput};
-                        Runtime.getRuntime().exec(commandAndArgs);
-                        System.out.println("[NEWS-DEBUG] Windows-User: " + commandAndArgs);
-                    } else {
-                        Runtime.getRuntime().exec("sh -c 'java -cp \"" + updatedAddon.getAbsolutePath() + "\" dev.janheist.newsaddon.modules.UpdateChecker \"" + minecraftDirectory + "\"'");
-                        ProcessBuilder processBuilder = new ProcessBuilder();
-                        processBuilder.command("sh", "-c", "java -cp \"" + updatedAddon.getAbsolutePath() + "\" dev.janheist.newsaddon.modules.UpdateChecker \"" + minecraftDirectory + "\"");
-                        Process process = processBuilder.start();
-
-                        StringBuilder output = new StringBuilder();
-                        BufferedReader reader = new BufferedReader(
-                                new InputStreamReader(process.getInputStream()));
-
-                        String line;
-                        while ((line = reader.readLine()) != null) {
-                            output.append(line).append("\n");
-                        }
-
-                        System.out.println("[NEWS-DEBUG] ProcessBuilder: " + output);
-
-
-                        System.out.println("[NEWS-DEBUG] No Windows-User: " + "sh -c 'java -cp \"" + updatedAddon.getAbsolutePath() + "\" dev.janheist.newsaddon.modules.UpdateChecker \"" + minecraftDirectory + "\"'");
-
-                    }
-
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private static void displayPrefix(String message) {
-        LabyMod.getInstance().displayMessageInChat("§7[§a§lNEWS§r§7] §r".concat(message));
     }
 }
