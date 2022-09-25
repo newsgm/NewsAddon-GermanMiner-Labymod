42d6d72cfbdc79980af38af51a0dacc1d88fddef
JanHeist <heist@rhrk.uni-kl.de>
2022-09-25 20:47:01 +0200

    Blitzer-Locations

--- src/main/java/dev/janheist/newsaddon/timer/SpeedWarning.java
+++ src/main/java/dev/janheist/newsaddon/timer/SpeedWarning.java
@@ -21,7 +21,20 @@ public class SpeedWarning extends TimerTask {
 
     public void initialize() {
         first = false;
-        speedCams.add(new BlockPos(-273, 80, 338));
+        speedCams.add(new BlockPos(-1002, 63, -2280));
+        speedCams.add(new BlockPos(-2450, 63, -2028));
+        speedCams.add(new BlockPos(-2303, 63, -1628));
+        speedCams.add(new BlockPos(-1756, 67, -1086));
+        speedCams.add(new BlockPos(-2916, 56, -1016));
+        speedCams.add(new BlockPos(-3147, 66, -52));
+        speedCams.add(new BlockPos(-2420, 66, 1037));
+        speedCams.add(new BlockPos(-2504, 67, 258));
+        speedCams.add(new BlockPos(-1532, 77, -81));
+        speedCams.add(new BlockPos(-1013, 65, -538));
+        speedCams.add(new BlockPos(-1347, 66, -1257));
+        speedCams.add(new BlockPos(-996, 63, -1400));
+        speedCams.add(new BlockPos(-787, 64, -2015));
+        speedCams.add(new BlockPos(301, 64, -1485));
     }
 
     @Override
@@ -52,7 +65,7 @@ public class SpeedWarning extends TimerTask {
                 }
             }
 
-            if (nearest != null && dist < 275) {
+            if (nearest != null && dist < 350) {
                 double d = nearest.getDistance(loc.getX(), loc.getY(), loc.getZ());
                 String distStr = String.format("%.1f", d);
                 Minecraft.getMinecraft().ingameGUI.setOverlayMessage("§c§lBlitzer in " + distStr + "m", true);
